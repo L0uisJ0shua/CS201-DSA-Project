0a6b53a06bd20a4c2a3e88aa200b39a5cbd5fe49
linsyhen99 <linsyhen99@gmail.com>
2021-10-26 11:20:45 +0800

    src/Utils/: Created a Latitude Logitude calculator to calculate the current location to the destination location difference in kilometers
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

---
+++ src/Utils/LatLongComparison.java
@@ -0,0 +1,31 @@
+package Utils;
+
+import java.util.*;
+import java.lang.*;
+
+public class LatLongComparison {
+    
+    public static double distanceDifference(double currLat, double currLong, double restaurantLat, double restaurantLong) {
+
+        // Conversion of latitudes and longitude to radians
+        // since the earth is a sphere and not FLAT!!!
+        currLat = Math.toRadians(currLat);
+        currLong = Math.toRadians(currLong);
+        restaurantLat = Math.toRadians(restaurantLat);
+        restaurantLong = Math.toRadians(restaurantLong);
+
+        // Using the Haversine formula to compute the distance in kilometers
+        double distanceLong = restaurantLong - currLong;
+        double distanceLat = restaurantLat - currLat;
+        double a = Math.pow(Math.sin(distanceLat / 2), 2)
+                 + Math.cos(currLat) * Math.cos(restaurantLat)
+                 * Math.pow(Math.sin(distanceLong / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius constant of the earth in Kilometers
+        double radiusInKm = 6371;
+
+        return c * radiusInKm;
+    }
+}

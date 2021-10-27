ed461082d573f248aa7f920e2c14ac0c2381c763
linsyhen99 <linsyhen99@gmail.com>
2021-10-28 01:19:42 +0800

    src/app/Main.java: Implemented an option for user to toggle whether they want to sort by rating or by distance
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- src/app/Main.java
+++ src/app/Main.java
@@ -31,6 +31,10 @@ public class Main {
         System.out.print("Acceptable distance(Km) --->  ");
         double acceptableRange = sc.nextDouble();
         sc.nextLine();
+
+        System.out.print("Do you prefer a higer rating restaurant? [Y/n] --->  ");
+        String sortingOption = sc.nextLine();
+
         // Hard code your current location's latitude and longitude here
         System.out.print("We recommend this: 39.778259,-105.417931. Use this? [Y/n] ");
         String res = sc.nextLine();
@@ -44,7 +48,7 @@ public class Main {
             System.out.print("Your current location longtitude ---> ");
             currLong = sc.nextDouble();
             sc.nextLine();
-            
+
         } else {
             currLat = 39.778259;
             currLong = -105.417931;
@@ -57,28 +61,32 @@ public class Main {
 
             System.out.println("Parsing JSON dataset into a tree map...");
             while ((line = reader.readLine()) != null) {
-                try{
+                try {
                     Restaurant restaurant = gson.fromJson(line, Restaurant.class);
 
                     // Comparing the opening hours of the restaurants
-                    if(restaurant.getHours().containsKey(dateTimeComparator.getDay()) && dateTimeComparator.isOpen(restaurant.getHours().get(dateTimeComparator.getDay()))){
-                        
-                        if (LatLongComparison.distanceDifference(currLat, currLong, restaurant.getLatitude(), restaurant.getLongitude()) <= acceptableRange) {
+                    if (restaurant.getHours().containsKey(dateTimeComparator.getDay())
+                            && dateTimeComparator.isOpen(restaurant.getHours().get(dateTimeComparator.getDay()))) {
+
+                        if (LatLongComparison.distanceDifference(currLat, currLong, restaurant.getLatitude(),
+                                restaurant.getLongitude()) <= acceptableRange) {
                             allRestaurant.put(restaurant.getName(), restaurant);
                             // System.out.println(restaurant.toString());
                         }
                         // System.out.println(restaurant.getHours().get("Monday"));
                         // break;
                     }
-                } catch(NullPointerException ex){
-                    
+                } catch (NullPointerException ex) {
+
                 }
             }
             System.out.println("Completed JSON -> tree map conversion.");
             System.out.println("Total time consumed to parse the entire dataset = "
-                    + String.format("%.10f", (System.currentTimeMillis() - time)/1000) + "\n\n");
+                    + String.format("%.10f", (System.currentTimeMillis() - time) / 1000) + "\n\n");
 
-            // // Bubble Sort
+            System.out.println("Total available data --> " + allRestaurant.size());
+
+            // Bubble Sort
             System.out.println("======= Commencing Bubble Sort Test ========");
 
             double start_time = System.currentTimeMillis();
@@ -86,22 +94,27 @@ public class Main {
             Restaurant[] top_rated = b.bucketSortStars(allRestaurant);
             Restaurant top_and_close = b.bubbleSortDistAndGet(top_rated, currLat, currLong);
             System.out.println(top_and_close.toString());
-            System.out.println("Total Time take to sort = " + String.format("%.10f", (System.currentTimeMillis() - start_time)/1000));
+            System.out.println("Total Time take to sort = "
+                    + String.format("%.10f", (System.currentTimeMillis() - start_time) / 1000));
             System.out.println("\n====== End of Bubble Sort Run ========\n");
-            
+
             // Merge Sorting
-            performMergeSort(allRestaurant, currLat, currLong);
-            
+            performMergeSort(sortingOption, allRestaurant, currLat, currLong);
+
         } catch (IOException e) {
             e.getStackTrace();
         }
     }
 
-    public static void performMergeSort(Map<String, Restaurant> restaurant, double currLat, double currLong){
+    public static void performMergeSort(String sortingOption, Map<String, Restaurant> restaurant, double currLat,
+            double currLong) {
         if (restaurant.size() == 0) {
             return;
         }
-        
+
+        // Initialise the merge sort function
+        MergeSort mergeSort = new MergeSort();
+
         System.out.println("======= Commencing Merge Sort Test ========");
         double startTime = System.currentTimeMillis();
 
@@ -109,15 +122,23 @@ public class Main {
         Restaurant[] restaurants = restaurant.values().toArray(new Restaurant[restaurant.values().size()]);
 
         System.out.println("Total size of data required to perform Merge Sort = " + restaurants.length);
-        
-        // Initialise the merge sort function
-        MergeSort mergeSort = new MergeSort();
-        // Perform Merge Sort on the given array
-        mergeSort.mergeSort(restaurants, 0, restaurants.length - 1, currLat, currLong);
+
+        if (sortingOption.equals("Y") || sortingOption.equals("y")) {
+            System.out.println("Sorting data based on rating...");
+            // Perform Merge Sort by highest rating and closest restaurant
+            mergeSort.mergeSortByRating(restaurants, 0, restaurants.length - 1, currLat, currLong);
+
+        } else {
+
+            System.out.println("Sorting data based on distance...");
+            // Perform Merge Sort by distance on the given array
+            mergeSort.mergeSortByDistance(restaurants, 0, restaurants.length - 1, currLat, currLong);
+        }
 
         // Display time taken to perform merge sort on the data given
-        System.out .println("Top restaurant availble = " + restaurants[0]);
-        System.out.println("Total time taken to perform Merge Sort = " + String.format("%.10f", (System.currentTimeMillis() - startTime)/1000));
+        System.out.println("Nearest restaurant availble = " + restaurants[0]);
+        System.out.println("Total time taken to perform Merge Sort = "
+                + String.format("%.10f", (System.currentTimeMillis() - startTime) / 1000));
         System.out.println("\n====== End of Merge Sort Test ========");
     }
 

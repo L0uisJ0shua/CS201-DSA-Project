7559687aa0e6bdf20d7152ebaf0f4f994985b5c9
Won Ying Keat <yingkeatwon@gmail.com>
2021-11-04 19:43:31 +0800

    Edited compile and run scripts

--- compile.sh
+++ compile.sh
@@ -1,3 +1,1 @@
 javac -d target/classes -cp "src/:lib/*" src/app/Main.java
-javac -d target/classes -cp "src/:lib/*" src/app/BasicTreeMapTest.java
-javac -d target/classes -cp "src/:lib/*" src/app/AVLTreeMapTest.java
